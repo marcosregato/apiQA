1adf203e2aa2325fb52fc8e626ad7dd00605a529
marcosregato <marcosregato01@gmail.com>
2021-08-12 11:30:36 -0300

    first commit

--- controller/projeto.controller.js
+++ controller/projeto.controller.js
@@ -1,14 +1,12 @@
 const ProjetoModel = require("../models/projeto.model.js");
 
 exports.create = (req, res) => {
-    // Validate request
   if (!req.body) {
       res.status(400).send({
       message: "Content can not be empty!"
     });
   }
 
-  // Create a Customer
   const projeto = new Projeto({
     id: req.body.id,
     nnome: req.body.nome,
